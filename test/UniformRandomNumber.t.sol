cfd3ebeca7784fd810b0525d636dc1d9e1ce34f1
Brendan Asselstine <mail.asselstine@gmail.com>
2023-08-15 12:00:19 -0700

    initial commit

---
+++ test/UniformRandomNumber.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.18;
+
+import "forge-std/Test.sol";
+import { console2 } from "forge-std/console2.sol";
+
+import { UniformRandomNumber } from "../src/UniformRandomNumber.sol";
+
+contract UniformRandomNumberTest is Test {
+
+  function test() external {
+    
+  }
+
+}
